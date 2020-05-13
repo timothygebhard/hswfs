bc451e96c82c0b1abc2a85787bf142d7b52260d0
Timothy Gebhard <mail@timothygebhard.de>
2020-05-13 22:16:17 +0200

    Add crop_center() function

--- hswfs/utils.py
+++ hswfs/utils.py
@@ -15,6 +15,38 @@ import numpy as np
 # FUNCTION DEFINITIONS
 # -----------------------------------------------------------------------------
 
+def crop_center(array: np.ndarray,
+                size: Tuple[int, ...]) -> np.ndarray:
+    """
+    Crop an n-dimensional array to the given size around its center.
+
+    Args:
+        array: The numpy array to be cropped.
+        size: A tuple containing the size of the cropped array. To not
+            crop along a specific axis, you can specify the size of
+            that axis as -1.
+    Returns:
+        The input array, cropped to the desired size around its center.
+    """
+
+    # Ensure that the the array shape and the size variable match
+    assert array.ndim == len(size), \
+        'Length of size must match number of dimensions of array!'
+
+    # Loop over the the axes of the array to create slices
+    slices = list()
+    for old_len, new_len in zip(array.shape, size):
+
+        # Compute start and end position for axis
+        start = old_len // 2 - new_len // 2 if new_len != -1 else None
+        end = start + new_len if start is not None else None
+
+        # Create a slice object for axis
+        slices.append(slice(start, end))
+
+    return array[tuple(slices)]
+
+
 def get_unit_disk_meshgrid(
     resolution: int,
 ) -> Tuple[np.array, np.array]:
