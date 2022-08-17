bf77b3bd9a256af53b2b951f2eb0360ba0c671d1
Timothy Gebhard <mail@timothygebhard.de>
2022-08-17 16:48:29 +0200

    Fix code style

--- demo/hswfs_example.py
+++ demo/hswfs_example.py
@@ -24,38 +24,38 @@ from hswfs.zernike import Wavefront, eval_cartesian
 # MAIN CODE
 # -----------------------------------------------------------------------------
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     # -------------------------------------------------------------------------
     # Set up virtual wavefront sensor, fit wavefront and compute PSF
     # -------------------------------------------------------------------------
 
     # Create a set of shifts for a HSWFS with a grid size of 8x8
-    print('Generating shifts...', end=' ', flush=True)
-    shifts = generate_test_shifts(test_case='defocus', grid_size=8)
-    print('Done!', flush=True)
+    print("Generating shifts...", end=" ", flush=True)
+    shifts = generate_test_shifts(test_case="defocus", grid_size=8)
+    print("Done!", flush=True)
 
     # Set up a new HSWFS instance
-    print('Setting up wavefront sensor...', end=' ', flush=True)
+    print("Setting up wavefront sensor...", end=" ", flush=True)
     sensor = HSWFS(relative_shifts=shifts)
-    print('Done!', flush=True)
+    print("Done!", flush=True)
 
     # Fit the wavefront that corresponds to the shifts
-    print('Fitting wavefront...', end=' ', flush=True)
+    print("Fitting wavefront...", end=" ", flush=True)
     coefficients = sensor.fit_wavefront(n_zernike=9)
     wavefront = Wavefront(coefficients=coefficients)
-    print('Done!', flush=True)
+    print("Done!", flush=True)
 
     # Compute the PSF that corresponds to that wavefront
-    print('Computing PSF...', end=' ', flush=True)
+    print("Computing PSF...", end=" ", flush=True)
     psf = sensor.get_psf(wavefront=wavefront, resolution=64)
-    print('Done!', flush=True)
+    print("Done!", flush=True)
 
     # -------------------------------------------------------------------------
     # Create a plot of the results
     # -------------------------------------------------------------------------
 
-    print('Plotting results...', end=' ', flush=True)
+    print("Plotting results...", end=" ", flush=True)
 
     # Set up a grid of plots for the results
     gs = gridspec.GridSpec(2, 2, width_ratios=[2.25, 1], height_ratios=[1, 1])
@@ -65,21 +65,29 @@ if __name__ == '__main__':
 
     # Plot the shifts on the wavefront sensor
     plot_shifts(ax=ax_sensor, relative_shifts=shifts)
-    ax_sensor.set_title('Shifts on wavefront sensor')
-    ax_sensor.set_aspect('equal')
+    ax_sensor.set_title("Shifts on wavefront sensor")
+    ax_sensor.set_aspect("equal")
 
     # Evaluate the wavefront on a grid and plot it
     x_0, y_0 = get_unit_disk_meshgrid(resolution=512)
     wf_grid = eval_cartesian(wavefront.cartesian, x_0=x_0, y_0=y_0)
     limit = 1.1 * np.nanmax(np.abs(wf_grid))
-    ax_wavefront.imshow(wf_grid, interpolation='nearest', cmap='RdBu_r',
-                        vmin=-limit, vmax=limit)
-    ax_wavefront.set_title('Wavefront')
+    ax_wavefront.imshow(
+        wf_grid,
+        interpolation="nearest",
+        cmap="RdBu_r",
+        vmin=-limit,
+        vmax=limit,
+    )
+    ax_wavefront.set_title("Wavefront")
 
     # Plot the PSF (cropped to center)
-    ax_psf.imshow(crop_center(psf, size=(33, 33)), interpolation='nearest',
-                  norm=colors.LogNorm(vmin=0.001, vmax=1))
-    ax_psf.set_title('PSF (log-scale)')
+    ax_psf.imshow(
+        crop_center(psf, size=(33, 33)),
+        interpolation="nearest",
+        norm=colors.LogNorm(vmin=0.001, vmax=1),
+    )
+    ax_psf.set_title("PSF (log-scale)")
 
     # Disable ticks on all subplots
     disable_ticks(ax_sensor)
@@ -88,6 +96,6 @@ if __name__ == '__main__':
 
     # Save the result
     plt.tight_layout()
-    plt.savefig('example_result.png', pad=0, dpi=300, bbox_inches='tight')
+    plt.savefig("example_result.png", pad=0, dpi=300, bbox_inches="tight")
 
-    print('Done!', flush=True)
+    print("Done!", flush=True)
--- demo/plot_zernike_polynomials.py
+++ demo/plot_zernike_polynomials.py
@@ -17,7 +17,7 @@ from hswfs.zernike import eval_cartesian, ZernikePolynomial
 # MAIN CODE
 # -----------------------------------------------------------------------------
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     # Define the maximum value of n
     n_max = 4
@@ -29,14 +29,17 @@ if __name__ == '__main__':
     for row, n in enumerate(range(0, n_max + 1)):
         for col, m in enumerate(range(-n, n + 1, 2)):
 
-            print(f'Running for Z^{m}_{n}...', end=' ', flush=True)
+            print(f"Running for Z^{m}_{n}...", end=" ", flush=True)
 
             # Create new Zernike polynomial
             zernike = ZernikePolynomial(m=m, n=n)
 
             # Add a new subplot to the plot
-            ax = plt.subplot2grid((n_max + 1, 2 * (n_max + 1)),
-                                  (n, n_max - n + 2 * col), colspan=2)
+            ax = plt.subplot2grid(
+                (n_max + 1, 2 * (n_max + 1)),
+                (n, n_max - n + 2 * col),
+                colspan=2,
+            )
 
             # Evaluate the Zernike polynomial on the unit disk
             x_0, y_0 = get_unit_disk_meshgrid(resolution=512)
@@ -44,15 +47,16 @@ if __name__ == '__main__':
             limit = np.nanmax(np.abs(img))
 
             # Actually plot the Zernike polynomial
-            ax.imshow(img, origin='lower', cmap='RdBu_r',
-                      vmin=-limit, vmax=limit)
+            ax.imshow(
+                img, origin="lower", cmap="RdBu_r", vmin=-limit, vmax=limit
+            )
 
             # Add plot options (title, disable ticks etc.)
-            ax.set_title(rf'$Z^{{{m}}}_{{{n}}}$', fontsize=4, pad=4)
-            ax.axis('off')
+            ax.set_title(rf"$Z^{{{m}}}_{{{n}}}$", fontsize=4, pad=4)
+            ax.axis("off")
 
-            print('Done!', flush=True)
+            print("Done!", flush=True)
 
     # Final plot options; save as PNG
     plt.tight_layout(h_pad=0.1, w_pad=0.1)
-    plt.savefig('zernike_polynomials.png', dpi=300, pad=0)
+    plt.savefig("zernike_polynomials.png", dpi=300, pad=0)
